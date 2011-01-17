c56cbcf7b4bbaa0248667743ad12ae5acc227583
faisal adnan <faisal.adnan@gmail.com>
2011-01-17 15:56:53 +0100

    Added com.mpe.financial.struts.form

---
+++ src/com/mpe/financial/struts/form/CustomFieldForm.java
@@ -0,0 +1,38 @@
+//---------------------------------------------------------
+// Application: Garage
+// Author     : Agung Hadiwaluyo
+// (Milan Fanz Club)
+// Copyright © 2005 MPE
+// Generated at Sat Sep 03 19:38:16 GMT+07:00 2005
+//---------------------------------------------------------
+
+package com.mpe.financial.struts.form;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.struts.action.ActionErrors;
+import com.mpe.common.form.*;
+import org.apache.struts.action.ActionMapping;
+
+public class CustomFieldForm extends BaseDynaForm {
+	private static final long serialVersionUID = 1L;
+
+	/** 
+	 * Method validate
+	 * @param ActionMapping mapping
+	 * @param HttpServletRequest request
+	 * @return ActionErrors
+	 */
+	public ActionErrors validate(ActionMapping mapping,HttpServletRequest request) {
+			return null;
+	}
+
+	/** 
+	 * Method reset
+	 * @param ActionMapping mapping
+	 * @param HttpServletRequest request
+	 */
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+
+	}
+	
+}
---
+++ src/com/mpe/financial/struts/form/UserForm.java
@@ -0,0 +1,53 @@
+//---------------------------------------------------------
+// Application: Garage
+// Author     : Agung Hadiwaluyo
+// (Milan Fanz Club)
+// Copyright © 2005 MPE
+// Generated at Sat Sep 03 19:38:16 GMT+07:00 2005
+//---------------------------------------------------------
+
+package com.mpe.financial.struts.form;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import com.mpe.common.form.*;
+import org.apache.struts.action.ActionMapping;
+
+/** 
+ * UserForm.java created by EasyStruts - XsltGen.
+ * http://easystruts.sf.net
+ * created on 06-05-2003
+ * 
+ * XDoclet definition:
+ * @struts:form name="userForm"
+ */
+public class UserForm extends BaseDynaForm {
+	private static final long serialVersionUID = 1L;
+
+	// --------------------------------------------------------- Instance Variables
+
+	// --------------------------------------------------------- Methods
+
+	/** 
+	 * Method validate
+	 * @param ActionMapping mapping
+	 * @param HttpServletRequest request
+	 * @return ActionErrors
+	 */
+	public ActionErrors validate(
+		ActionMapping mapping,
+		HttpServletRequest request) {
+			return null;
+	}
+
+	/** 
+	 * Method reset
+	 * @param ActionMapping mapping
+	 * @param HttpServletRequest request
+	 */
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+
+	}
+
+}
